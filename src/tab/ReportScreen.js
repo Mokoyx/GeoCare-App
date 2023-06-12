6509258380040aa64626b2c4d22ee392de6c611c
Mokoy <kim.carmelotes01@gmail.com>
2023-06-13 07:09:00 +0800

    nothing added just corrected

--- src/tab/ReportScreen.js
+++ src/tab/ReportScreen.js
@@ -124,8 +124,7 @@ export default function CameraScreen() {
       </TouchableOpacity>
       
       <TouchableOpacity style={styles.button} onPress={() => handleButtonPress("Traffic")}>
-        <Ionicons name="car-outline" size={30} color="darkcyan
-        " />
+        <Ionicons name="car-outline" size={30} color="darkcyan" />
         <Text style={{color: "grey", fontSize: 18, fontWeight: "bold"}}>Traffic Issues</Text>
       </TouchableOpacity>
       </SafeAreaView>
